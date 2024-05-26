271dfad1a477402728a13787faa3986ad21f39e4
ankur94516 <ankur@gmail.com>
2024-05-26 17:07:25 +0530

    leae types added; migration applied

---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementSystem.Web.Data;
+
+public class LeaveType
+{
+    public int Id { get; set; }
+
+    [MaxLength(50,ErrorMessage ="Length out of bound")]
+    public string Name { get; set; }
+    public int NumberOfDays { get; set; }
+}
